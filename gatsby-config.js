d4933f1a76f5458dceb9395870ce44a7c8af7805
Eduardo S. Libardi <eslibardi@gmail.com>
2022-04-05 11:07:29 -0300

    set ssr and browser

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,4 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,

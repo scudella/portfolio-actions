0345ca6490500db9bea3d32b5d57b8909cf23ef9
Eduardo S. Libardi <eslibardi@gmail.com>
2022-04-13 14:22:43 -0300

    contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+const contact = () => {
+  return (
+    <>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>get in touch</h3>
+          <form action="https://formspree.io/f/mjvlarvz" method="POST">
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="form-control"
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="form-control"
+              />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="message"
+                className="form-control"
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-btn btn">
+              submit here
+            </button>
+          </form>
+        </article>
+      </section>
+    </>
+  )
+}
+
+export default contact
